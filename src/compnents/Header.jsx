ea8b915c1efe6c94092b4c8f87a8a62066d0766b
Shahid Adnan Wali <walishahidadnan@gmail.com>
2023-09-15 23:49:54 +0500

    added navigation to check and run navigation with react routing

--- src/compnents/Header.jsx
+++ src/compnents/Header.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import Navbar from './Navbar/Navbar'
 import Product from './prodact/product'
+import { Routes, Route } from 'react-router-dom'
+
 const Header = () => {
   return (
     <>
       <Navbar />
-      <Product />
+
+      <Routes>
+        <Route path='/' element={<Product />} />
+      </Routes>
+      
     </>
   )
 }
