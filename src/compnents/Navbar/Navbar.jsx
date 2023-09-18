7bb5a2a5bf78f9bd29eff55d037133cb7ed3e086
Shahid Adnan Wali <walishahidadnan@gmail.com>
2023-09-19 00:35:57 +0500

    removed notifiction

--- src/compnents/Navbar/Navbar.jsx
+++ src/compnents/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ const navigation = [
   { name: 'Dashboard', href: '/', current: true },
   { name: 'Promo', href: '/promo', current: false },
   { name: 'Team', href: '/team', current: false },
-  { name: 'Calendar', href: '#', current: false },
+  { name: 'Calendar', href: '#', current: false }
 ]
 
 function classNames(...classes) {
@@ -60,14 +60,6 @@ export default function Navbar() {
                 </div>
               </div>
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                <button
-                  type="button"
-                  className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                >
-                  <span className="absolute -inset-1.5" />
-                  <span className="sr-only">View notifications</span>
-                  <BellIcon className="h-6 w-6" aria-hidden="true" />
-                </button>
 
                 {/* Profile dropdown */}
                 <Menu as="div" className="relative ml-3">
