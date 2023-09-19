8f18554b3b15df101249c46d49ffff6d5095bb41
Shahid Adnan Wali <walishahidadnan@gmail.com>
2023-09-19 19:59:10 +0500

    arranged folder and files and added header section

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 // import './App.css'
-import Header from './Pages/Header'
+import Home from './Pages/Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Header />
+      <Home />
     </>
   )
 }
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,24 +2,29 @@ import React from 'react'
 import Navbar from '../compnents/Navbar/Navbar'
 import Product from '../compnents/prodact/product'
 import Promo from '../compnents/Promo/Promo'
-import { Routes, Route } from 'react-router-dom'
+import Header from '../compnents/Header/Header'
+// import { Routes, Route } from 'react-router-dom'
 import Team from '../compnents/Team/Team'
 import Content from '../compnents/Content/Content'
 
-const Header = () => {
+const Home = () => {
   return (
     <>
       <Navbar />
 
-      <Routes>
+      {/* <Routes>
         <Route path='/' element={<Product />} />
         <Route path='/promo' element={<Promo />} />
         <Route path='/team' element={<Team />} />
         <Route path='/promo/content' element={<Content />} />
-      </Routes>
-      
+      </Routes> */}
+      <Header />
+      <Product />
+      <Promo />
+      <Team />
+      <Content />
     </>
   )
 }
 
-export default Header
+export default Home
