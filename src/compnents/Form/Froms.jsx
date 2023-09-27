442dbc399e61804f7855b3ebd258abcb50d5067a
Shahid Adnan Wali <walishahidadnan@gmail.com>
2023-09-27 19:51:44 +0500

    some changes in form

--- src/compnents/Form/Froms.jsx
+++ src/compnents/Form/Froms.jsx
@@ -3,7 +3,7 @@ import { PhotoIcon, UserCircleIcon } from '@heroicons/react/24/solid'
 export default function Froms() {
   return (
     <form>
-      <div className="space-y-12 bg-white mx-auto align-center p-5 text-white">
+      <div className="space-y-12 bg-white mx-auto p-5 text-white">
         <div className="border-b border-gray-900/10 pb-12 text-white w-3/6 mx-auto"> 
           <h2 className="text-base font-semibold leading-7 text-gray-900">Profile</h2>
           <p className="mt-1 text-sm leading-6 text-gray-600">
@@ -17,37 +17,47 @@ export default function Froms() {
               </label>
               <div className="mt-2">
                 <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                  <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm">workcation.com/</span>
                   <input
                     type="text"
                     name="username"
                     id="username"
                     autoComplete="username"
-                    className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6 bg-white"
+                    className="block flex-1 border-0 bg-transparent py-1.5 pl-2 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6 bg-white"
                     placeholder="janesmith"
                   />
                 </div>
               </div>
             </div>
-
-            <div className="col-span-full">
-              <label htmlFor="about" className="block text-sm font-medium leading-6 text-gray-900">
+            <div className="sm:col-span-4">
+              <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
+                Password
+              </label>
+              <div className="mt-2">
+                <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600">
+                  <input
+                    type="password"
+                    name="password"
+                    id="username"
+                    autoComplete="username"
+                    className="block flex-1 border-0 bg-transparent py-1.5 pl-2 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6 bg-white"
+                    placeholder="janesmith"
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="sm:col-span-3">
+              <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
                 About
               </label>
               <div className="mt-2">
-                <textarea
-                  id="about"
-                  name="about"
-                  rows={3}
-                  cols={10}
-                  className="block w-500 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  defaultValue={''}
-                />
+                <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600">
+                  <textarea name="message" id="message" cols="33" rows="10" className='bg-blue'></textarea>
+                </div>
               </div>
-              <p className="mt-3 text-sm leading-6 text-gray-600">Write a few sentences about yourself.</p>
             </div>
-
+            
           </div>
+          <button className='rounded p-4 mt-4 bg-blue-800 hover:bg-blue-500'>Submit</button>
         </div>
       </div>
     </form>
