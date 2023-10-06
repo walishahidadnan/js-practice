5586b869b0d07941c29d7b0b338d3464d66f62b9
Shahid Adnan Wali <walishahidadnan@gmail.com>
2023-10-06 19:49:36 +0500

    changed

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,19 +1,11 @@
 import React from 'react'
 import Navbar from '../compnents/Navbar/Navbar'
-import Header from '../compnents/Header/Header'
 
 const Home = () => {
   return (
     <>
       <Navbar />
 
-      {/* <Routes>
-        <Route path='/' element={<Product />} />
-        <Route path='/promo' element={<Promo />} />
-        <Route path='/team' element={<Team />} />
-        <Route path='/promo/content' element={<Content />} />
-      </Routes> */}
-      <Header />
 
     </>
   )
