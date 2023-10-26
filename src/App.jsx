f1763757697420ce50aae4b2888b3dd10d88b980
Shahid Adnan Wali <walishahidadnan@gmail.com>
2023-10-26 19:33:47 +0500

    completed weather app in react and typescript

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
 import { useState } from 'react'
-import Weather from './components/weatherapp/Weather'
+import Weather2 from './components/weatherapp/Weather2'
 // import TodoList from './components/todolist/TodoList'
 // import TodoList2 from './components/todolist/TodoTwo';
 function App() {
 
   return (
     <>
-    {/* simple React TodoList */}
+    {/*  React TodoList */}
       {/* <TodoList />  */}
     {/* Typescript Todolist */}
       {/* <TodoList2 /> */}
-    
-    <Weather />
+    {/* weather app in react */}
+      {/* <Weather /> */}
+    {/* weather app in typescript  */}
+    <Weather2 />
+
     </>
   )
 }
