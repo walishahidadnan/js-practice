2001f95b00ffcd30b8b599ed8d3052ab0ee5709d
Shahid Adnan Wali <walishahidadnan@gmail.com>
2023-10-27 19:04:09 +0500

    updated

--- src/components/TictoeGame/TicToe.jsx
+++ src/components/TictoeGame/TicToe.jsx
@@ -6,23 +6,39 @@ import './tictoe.css'
 const Player_X = 'X';
 const Player_O = 'O'
 
-const winningCOmbination = [
+const winningCombinations = [
+  //Rows
+  { combo: [0, 1, 2], strikeClass: "strike-row-1" },
+  { combo: [3, 4, 5], strikeClass: "strike-row-2" },
+  { combo: [6, 7, 8], strikeClass: "strike-row-3" },
 
-  {combo: [0, 1, 2], strikeClass: "strike-row-1"},
-  {combo: [3, 4, 5], strikeClass: "strike-row-2"},
-  {combo: [6, 7, 8], strikeClass: "strike-row-3"},
-  {combo: [0, 3, 6], strikeClass: "strike-row-1"},
-  {combo: [1, 4, 7], strikeClass: "strike-row-2"},
-  {combo: [2, 5, 8], strikeClass: "strike-row-3"},
+  //Columns
+  { combo: [0, 3, 6], strikeClass: "strike-column-1" },
+  { combo: [1, 4, 7], strikeClass: "strike-column-2" },
+  { combo: [2, 5, 8], strikeClass: "strike-column-3" },
+
+  //Diagonals
+  { combo: [0, 4, 8], strikeClass: "strike-diagonal-1" },
+  { combo: [2, 4, 6], strikeClass: "strike-diagonal-2" },
+];
 
 
-]
+function checkWinner(tiles, setStrikeClass) {
+    for (const { combo, strikeClass } of winningCombinations) {
+      const tileValue1 = tiles[combo[0]];
+      const tileValue2 = tiles[combo[1]];
+      const tileValue3 = tiles[combo[2]];
+      console.log("Tile Values:", tileValue1, tileValue2, tileValue3); 
+      if (
+        tileValue1 != null &&
+        tileValue1 === tileValue2 &&
+        tileValue2 === tileValue3
+      ) {
+        setStrikeClass(strikeClass);
+      }
+    }
 
-
-
-function checkWinner() {
-  console.log('check winner')
-}
+  }
 
 
 const TicToe = () => {
@@ -46,7 +62,7 @@ const TicToe = () => {
   }
 
   useEffect(()=>{
-    checkWinner()
+    checkWinner();
   }, [tiles])
 
   return (
